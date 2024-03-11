df7026477bf5e28cad3bb7983705797a7e6f6fe5
SnowDesert <138176996+Snowdeserr@users.noreply.github.com>
2024-03-12 04:58:35 +1300

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,19 @@
+let TechnikLampe = 0
+input.onButtonPressed(Button.A, function () {
+    if (TechnikLampe == 0) {
+        TechnikLampe = 1
+    } else {
+        TechnikLampe = 0
+    }
+})
+basic.forever(function () {
+    if (TechnikLampe == 0) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(pins.analogReadPin(AnalogPin.P0) * 2)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    } else if (TechnikLampe == 1) {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(pins.analogReadPin(AnalogPin.P0) * 6)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+})
